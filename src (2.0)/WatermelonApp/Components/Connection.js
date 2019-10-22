bd55521d17b20a549a67275026d0c00ed1e9db23
bastien506 <33631882+bastien506@users.noreply.github.com>
2019-10-23 01:56:20 +0200

    Connection + beginning wallet

---
+++ src (2.0)/WatermelonApp/Components/Connection.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Redirect, Link } from 'react-router-dom';
+
+import SignIn from './SignIn';
+import SignUp from './SignUp'
+
+import '../style/connection.css';
+import signIn from '../img/sign_in.png';
+import signUp from '../img/sign_up.png';
+import about from '../img/about.png';
+import { isTSTypeAliasDeclaration } from '@babel/types';
+
+
+
+
+class Connection extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            render: '',
+            connected: false,
+            showMe: true
+        }
+        this.handleChangeValue = this.handleChangeValue.bind(this);
+    }
+    
+    handleChangeValue = (connected) => {
+        if (connected == true) {
+            console.log(this);
+            console.log(this.props);
+            this.props.value(true);
+        } else {
+            this.setState({showMe:true})
+        }
+    }
+
+
+    handleClick(compName, e) {
+        console.log(compName);
+        this.setState({ render: compName });
+        this.setState({ showMe: !this.state.showMe })
+    }
+
+    _renderSubComp() {
+        switch (this.state.render) {
+            case 'signin': return <SignIn connected={this.handleChangeValue} display={true} showMe={this.state.showMe}/>
+            case 'signup': return <SignUp connected={this.handleChangeValue} display={true} showMe={this.state.showMe}/>
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.showMe &&
+                    <div className="connection-header">
+                        <div className="option-section" onClick={this.handleClick.bind(this, 'signin')}>
+                            <img src={signIn} className="logo" />Sign in
+                    </div>
+                        <div className="option-section" onClick={this.handleClick.bind(this, 'signup')}>
+                            <img src={signUp} className="logo" />Sign up
+                    </div>
+                        <div className="option-section" onClick={this.handleClick.bind(this, 'about')}>
+                            <img src={about} className="logo" />About Watermelon
+                    </div>
+                    </div>
+                }
+                {this._renderSubComp()}
+            </div>
+
+        );
+
+    }
+}
+
+export default Connection;
+
+
---
+++ src (2.0)/WatermelonApp/Components/SignUp.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+
+import Connection from "./Connection"
+
+import signup from "../style/signup.css";
+import { Redirect, Link } from 'react-router-dom';
+
+import username from "../img/username.png";
+import psw from "../img/psw.png";
+import name from "../img/name.png"
+import { LocalStorageGetter, LocalStorageSetter } from '../Shortcut';
+
+
+class SignUp extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            render: '',
+            id: 1,
+            lastname: "",
+            firstname: "",
+            email: "",
+            password: "",
+            is_admin: false
+        }
+    }
+
+    dismiss() {
+        this.props.unmountMe();
+    }
+
+    back = () => {
+        this.setState({back:true});
+        this.props.connected(false);
+    }
+
+    saveData = (data) => {
+        this.setState({
+            [data.target.name]: data.target.value
+        });
+    }
+
+    AccountCreation = () => {
+
+        let users = LocalStorageGetter("users");
+        var creavalide = true;
+        users.map((user) => {
+            //Verification que l'email n'est pas deja inscrite
+            if (user.email == this.state.email) { creavalide = false }
+        });
+        //Mise en place de l'ID
+        users.map((user) => {
+            this.state.id++;
+        });
+        if (creavalide == true) {
+            let newUser = { id: this.state.id, last_name: this.state.last_name, first_name: this.state.first_name, email: this.state.email, password: this.state.password, is_admin: this.state.is_admin };
+            LocalStorageSetter("last_name", newUser.last_name);
+            LocalStorageSetter("first_name", newUser.first_name);
+            LocalStorageSetter("email", newUser.email);
+            LocalStorageSetter("password", newUser.password);
+            let allUsers = LocalStorageGetter("users");
+            allUsers.push(newUser);
+            console.log(newUser);
+            LocalStorageSetter("users", allUsers);
+            alert("Creation valide !");
+            this.setState({ connected: true });
+            this.setState({back:true});
+            this.props.connected(true);
+        }
+    }
+
+    testBDD = () => {
+        let users = LocalStorageGetter("users");
+        console.log(users)
+    }
+
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                {this.props.display && !this.props.showMe &&
+                    <div className="signup-form">
+                        <div className="option-section">
+                            Please provide your information to sign-up
+                        </div>
+                        <div className="option-section">
+                            <img src={name} className="logo" /><span>Last name</span>
+                            <input className="input-entry" name="last_name" onChange={this.saveData} type="text"></input>
+                        </div>
+                        <div className="option-section">
+                            <img src={name} className="logo" /><span>First name</span>
+                            <input className="input-entry" name="first_name" onChange={this.saveData} type="text"></input>
+                        </div>
+                        <div className="option-section">
+                            <img src={username} className="logo" /><span>Email</span>
+                            <input className="input-entry" name="email" onChange={this.saveData} type="email"></input>
+                        </div>
+                        <div className="option-section">
+                            <img src={psw} className="logo" /><span>Password</span>
+                            <input className="input-entry" name="password" onChange={this.saveData} type="password"></input>
+                        </div>
+                        <div className="button-list">
+                            <button onClick={this.AccountCreation}>Sign-up</button>
+                            <button onClick={this.back}>Back</button>
+                            <button onClick={this.testBDD}>Test BDD</button>
+                        </div>
+                    </div>}
+            </div>
+        );
+    }
+}
+
+export default SignUp;
