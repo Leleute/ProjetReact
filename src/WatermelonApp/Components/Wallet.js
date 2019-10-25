dd55436af1c6f357393a7fa324a8a82e3a9db35c
bastien506 <33631882+bastien506@users.noreply.github.com>
2019-10-25 16:09:49 +0200

    fixed

--- src/WatermelonApp/Components/Wallet.js
+++ src/WatermelonApp/Components/Wallet.js
@@ -9,7 +9,7 @@ import '../style/wallet.css';
 
 import icoOverall from '../img/ico_overall.png';
 import icoWallet from '../img/ico_wallet.png';
-import icoExchange from '../img/ico_exchange.png';
+import icoTransfer from '../img/ico_exchange.png';
 import icoHistory from '../img/ico_history.png';
 import icoSettings from '../img/ico_settings.png';
 import icoSignOut from '../img/ico_sign_out.png';
@@ -54,7 +54,7 @@ class Wallet extends Component {
                         </div>
                         <li onClick={this.changeMenu.bind(this, 0)}><img src={icoOverall} />Overall</li>
                         <li onClick={this.changeMenu.bind(this, 1)}><img src={icoWallet} />Wallet</li>
-                        <li onClick={this.changeMenu.bind(this, 2)}><img src={icoExchange} />Exchange</li>
+                        <li onClick={this.changeMenu.bind(this, 2)}><img src={icoTransfer} />Transfer</li>
                         <li onClick={this.changeMenu.bind(this, 3)}><img src={icoHistory} />History</li>
                         <div className="list-bottom">
                             <li onClick={this.changeMenu.bind(this, 4)}><img src={icoSettings} />Account</li>
