66b9be8de52644da1aaab635dccf065fbbec6dbd
bastien506 <33631882+bastien506@users.noreply.github.com>
2019-10-25 16:34:13 +0200

    fixed

--- src/WatermelonApp/Components/TransferMenu.js
+++ src/WatermelonApp/Components/TransferMenu.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
 import icoTransfer from '../img/ico_exchange.png';
-
+import icoTransferReceiver from '../img/ico_transfer_receiver.png';
+import icoUsername from "../img/username.png";
+import icoEmail from "../img/name.png";
 import { LocalStorageGetter } from '../Shortcut';
 
 class TransferMenu extends Component {
@@ -14,12 +16,26 @@ class TransferMenu extends Component {
     }
 
     render() {
-        console.log("wallet")
-
         return (
             <div className="container">
                 <header><img src={icoTransfer} className="ico" />TRANSFER</header>
+                <section>
+                    <div className='section-header'><img src={icoTransferReceiver} className="ico" /><span >Choose a Transfer receiver</span></div>
+                    {LocalStorageGetter('users').map(function (object, i) {
+                        return (
+                            <div className="user-choice">
+                                {object.id != LocalStorageGetter('connectedUser').id &&
+                                    <div className="item">
+                                        <li><img src={icoUsername} className="ico" /> <p className="display-value">{object.last_name.toUpperCase()} {object.first_name}</p></li>
+                                        <li><img src={icoEmail} className="ico" /><p className="display-value"> {object.email}</p></li>
+                                    </div>
+                                }
+                            </div>
+                        );
+                    }, this)}
+                </section>
             </div>
+
         );
     }
 }
